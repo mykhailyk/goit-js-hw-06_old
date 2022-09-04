0fa02489fcde8b2f81d744b2ddb5ea5e486981b5
Yurii Mikhaylik <imxomunn@gmail.com>
2022-09-04 12:42:04 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const counterRef = document.querySelector(".counter");
+
+const decrementButton = counterRef.querySelector(
+  'button[data-action="decrement"]'
+);
+const incrementButton = counterRef.querySelector(
+  'button[data-action="increment"]'
+);
+const valueEl = counterRef.querySelector("#value");
+
+const counter = {
+  value: 0,
+  decrement() {
+    this.value -= 1;
+  },
+  increment() {
+    this.value += 1;
+  },
+};
