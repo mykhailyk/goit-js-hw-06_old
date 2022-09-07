83d512c2399d964f603d826bf411f56eab66c512
Yurii Mikhaylik <imxomunn@gmail.com>
2022-09-07 16:37:00 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", whenFormSubmit);
 
+function whenFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+
+  const formResult = {};
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, name) => {
+    formResult[name] = value;
+  });
+
+  form.reset();
+}
