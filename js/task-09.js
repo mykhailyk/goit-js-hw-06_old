4438b5087039b763e140907df30093fc2c476a44
Yurii Mikhaylik <imxomunn@gmail.com>
2022-09-08 11:46:52 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const spanRef = document.querySelector(".color");
+const userBtn = document.querySelector(".change-color");
+
+userBtn.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  const randomColor = getRandomHexColor();
+
+  bodyRef.style.backgroundColor = `${randomColor}`;
+  spanRef.textContent = bodyRef.style.backgroundColor;
+}
